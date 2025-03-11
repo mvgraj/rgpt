5db3b9adeaf6d828d3b3d93699c59fcd6b70ce78
sruthiyadav <sruthiyadav.b99@gmail.com>
2025-03-11 18:18:23 +0530

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,26 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-// import { defineAuth, secret } from '@aws-amplify/backend';
-
-// const firebaseConfig = {
-//   apiKeyapiKey: "AIzaSyCMQ_jRFLGwo-e3GP3vadPHuRIXk1ZcI9Q",
-//   authDomain: "research-gpt-d84e3.firebaseapp.com",
-//   projectId: "research-gpt-d84e3",
-//   storageBucket: "research-gpt-d84e3.firebasestorage.app",
-//   messagingSenderId: "667211054303",
-//   appId: "1:667211054303:web:32c917d3bcf9d33830bd6a",
-//   measurementId: "G-9NZVZ3XZDX"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-// const db = getFirestore(app);
-
-// export { auth, db };
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
