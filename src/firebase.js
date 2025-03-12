ba29594d6ce850b8520f2df47cb5755226b17062
mvgraj <mugivijaygovindraj@outllook.com>
2025-03-12 08:03:42 +0530

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Define constants for each environment variable
-const FIREBASE_API_KEY = process.env.REACT_APP_FIREBASE_API_KEY;
-const FIREBASE_AUTH_DOMAIN = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
-const FIREBASE_PROJECT_ID = process.env.REACT_APP_FIREBASE_PROJECT_ID;
-const FIREBASE_STORAGE_BUCKET = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET;
-const FIREBASE_MESSAGING_SENDER_ID = process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID;
-const FIREBASE_APP_ID = process.env.REACT_APP_FIREBASE_APP_ID;
-const FIREBASE_MEASUREMENT_ID = process.env.REACT_APP_FIREBASE_MEASUREMENT_ID;
+const FIREBASE_API_KEY = process.env.VITE_FIREBASE_API_KEY;
+const FIREBASE_AUTH_DOMAIN = process.env.VITE_FIREBASE_AUTH_DOMAIN;
+const FIREBASE_PROJECT_ID = process.env.VITE_FIREBASE_PROJECT_ID;
+const FIREBASE_STORAGE_BUCKET = process.env.VITE_FIREBASE_STORAGE_BUCKET;
+const FIREBASE_MESSAGING_SENDER_ID = process.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
+const FIREBASE_APP_ID = process.env.VITE_FIREBASE_APP_ID;
+const FIREBASE_MEASUREMENT_ID = process.env.VITE_FIREBASE_MEASUREMENT_ID;
 
 // Log the environment variables to ensure they are loaded correctly
 console.log('Firebase API Key:', FIREBASE_API_KEY);
